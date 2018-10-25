2dd77c749dacd220a79ccd6d44ed8a90f48e396a
mj9812 <43096689+mj9812@users.noreply.github.com>
2018-10-25 10:21:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Homework Assignment 1
+
+First Home Work Assignment on the Node.js Master Class.
+
+## Project Description
+A simple "Hello World" RESTful JSON API that listens on a port provided by the user.
+When someone posts anything to the route /hello, it will return a welcome message, in JSON format.
+
+## Install and Run
+
+* Clone this repository
+* Open a Terminal, Go to HW-Assignment1 folder and run command `node index.js`
+* Open [http://localhost:3000/hello](http://localhost:3000/hello) to view it in the browser.
+* Or use Postman to send a request to http://localhost:3000/hello to receive the JSON object.
---
+++ index.js
@@ -0,0 +1,45 @@
+// Import required libs
+const http = require('http');
+const url = require('url');
+
+// Create and Configure the Server
+const server = http.createServer((req, res) => 
+{
+    // Get the Url, Path, Method and Headers
+    const parseUrl = url.parse(req.url, true);
+    const path = parseUrl.pathname.replace(/^\/+|\/+$/g, '');
+    const { method, headers } = req;
+
+    // Create a new Request Handler and find the Requested Route
+    const router = new handlers();
+    const currentHandler = router[path] ? router[path] : router.notFound;
+
+    // Prepare the data to be sent 
+    const data = { method, url, path, headers };
+
+    // Send the response back for the http request
+    currentHandler(data, (statusCode, message) => {
+        res.writeHead(statusCode, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(message));
+        res.end();
+    });
+
+});
+
+// Start the Server on port 3000 and Listen for Incoming Requests.
+server.listen(3000, () =>{
+    console.log('The server is running and listening on port 3000');
+});
+
+// Create Handlers for Incoming Requests.
+class handlers
+{
+    hello(data, callback)
+    {
+        callback(200, { 'message': 'Hello World. Greetings From Node.js Master Class Assignment 1' });
+    }
+    notFound(data, callback)
+    {
+        callback(404, { 'message': 'The requested page was not found.' });
+    }
+}
